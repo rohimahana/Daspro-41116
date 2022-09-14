5a6dada57181bb27e9c9fd4e5d6c73ef2e005796
rohimahana <113038377+rohimahana@users.noreply.github.com>
2022-09-14 11:00:55 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Halo nama saya Rohima Choirul Hana Nim : A11.2022.14603 \nSaya lahir di Kota Pekalongan pada tanggal 07 Mei 2004." << endl;
+    cout << "Hobi saya yaitu bermain voli,makan,dan tidur. \nSaya mempunyai hewan peliharaan yaitu Sugar Glider. \nSekian biodata yang bisa saya tuliskan. \nTerima Kasih." <<endl;
+    return 0;
+}
